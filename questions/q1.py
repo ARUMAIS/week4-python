ef6041932da28e76efbd0cc1709c54aa6bc6d2dd
Aishwaryalakshmi Arumugam <aishwaryalakshmi.a@gmail.com>
2021-10-15 10:21:09 +0100

    updated

--- questions/q1.py
+++ questions/q1.py
@@ -7,17 +7,27 @@
 # Licensing      : (c) 2021 Aishwarya,LYIT
 #                  Available under GNU Public License (GPL)
 # Description    
-# ---------------------------
+# System information
+# using platform and os module
 """
-import platform
-import os
+import platform  #import the random module
+import os        #for the operating system
 
 def print_config_details():
+    '''
+       Going to print the system configuration details
+       Parameters:
+       none
+       Returns:
+       none
+    '''
+    print("-" * 68)
     print("Platform processor:", platform.processor())
     print("Machine type:", platform.machine())
     print("Network name:", platform.node())
     print("Operating system:", platform.system())
     print("Platform architecture:", platform.architecture())
     print("PATH value:", os.environ['PATH'])
+    print("-" * 68)
 if __name__ == "__main__":
     print_config_details()
